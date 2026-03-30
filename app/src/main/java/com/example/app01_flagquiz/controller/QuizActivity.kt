37b157c77822fc3b16dd5c700295af059ac63856
Joelinton Dorte <joelinton.dorte@ufpr.br>
2026-03-30 18:29:33 -0300

    feat: Montagem inicial layout

--- app/src/main/java/com/example/app01_flagquiz/controller/QuizActivity.kt
+++ app/src/main/java/com/example/app01_flagquiz/controller/QuizActivity.kt
@@ -1,16 +1,17 @@
-package com.example.app01_flagquiz
+package com.example.app01_flagquiz.controller
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.app01_flagquiz.R
 
-class MainActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quiz)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
